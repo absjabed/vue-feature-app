71e14b094b9c00f5ef7d095e0eaa95c27d405c62
absjabed <absjbd@gmail.com>
2018-12-04 22:36:31 +0600

    vue app initial commit till mock items..

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    
+    <navbar></navbar>
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <inventory :items="items"></inventory>
+      </div>
+      <div class="col-sm-2">
+        <cart></cart>
+      </div>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Inventory from './components/Inventory'
+import data from './data.js'
+
+export default {
+  components:{
+    Navbar,
+    Cart,
+    Inventory
+  },
+  data (){
+    return{
+      items: []
+    }
+  },
+  mounted (){
+    this.items = data
+  }
+}
+</script>
+
+<style>
+.container {
+  padding-top: 10px;
+}
+</style>
